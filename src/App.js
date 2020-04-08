a7b01ce34fbfddc185a09509ab756fe0e449bdf2
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-08 15:25:34 -0700

    Created toDoItems list in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+const toDoItems = [
+  {
+    name: "Clean Bedroom",
+    id: 1,
+    completed: false
+  },
+  {
+    name: "Make Dinner",
+    id: 2,
+    completed: false
+  },
+  {
+    name: "Change Engine Oil",
+    id: 3,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
