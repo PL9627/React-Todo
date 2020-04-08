59df2c0089f72cd979de23ff60486624ba00eced
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-08 16:28:06 -0700

    added handleChanges to ToDoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 class ToDoForm extends React.Component {
-    
+    constructor() {
+        super();
+
+        this.state = {
+            item: ""
+        };
+    }
+
+    handleChanges = e => {
+        this.setState({ [e.target.name]: e.target.value });
+
+        console.log(this.state.item);
+    }
+
+    submitItem = e => {
+        e.preventDefault();
+
+        this.setState({item: ""});
+
+        this.props.addToDoItem(e, this.state.item);
+    };
+
+    render() {}
 };
 
 export default ToDoForm;
