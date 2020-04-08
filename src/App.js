ef9b91e8fdfde033ad43eeb362a12b32d8896282
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-08 15:27:28 -0700

    added constructor to App in App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,13 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      toDoItems
+    };
+  }
+
   render() {
     return (
       <div>
