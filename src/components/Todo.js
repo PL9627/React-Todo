b480fef1d86a2e03601ddf4ef165c179ae86b395
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-08 16:07:05 -0700

    ToDo fully fleshed out in ToDo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,12 @@ import React from 'react';
 const ToDo = props => {
     console.log("ToDo props:", props)
     return(
-        <div></div>
+        <div
+        classname = {`item${props.item.completed ? "completed" : ""}`}
+        onClick = {() => props.toggleToDoItem(item.id)}
+        >
+            <p>{props.item.name}</p>
+        </div>
     )
 };
 
